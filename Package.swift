20cd3910737742d5fb0cfb4fd643461d75a87438
Fabio de Albuquerque Dela Antonio <fabio914@gmail.com>
2021-03-07 04:15:38 +0000

    Specifying platforms

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "FFmpeg",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(name: "FFmpeg", targets: ["FFmpeg"]),
     ],
